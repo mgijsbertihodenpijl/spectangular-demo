af234df2ce543f31018711ee833d07e8329d9122
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-07-20 17:19:36 +0200

    specs for components

---
+++ angular-material/dist/spec.input.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//use form with ng-model
+// user.name (input) ,
+// user.address2 (input),
+// user.city (input),
+// user.state (input),
+// user.postalCode (input)
+// user.biography (area)
