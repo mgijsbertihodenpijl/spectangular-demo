b0609ea2bcfefd56aaa80fd563898133aaa8b08a
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-07-21 10:36:06 +0200

    removed obsolete files

--- angular-material/dist/conf.button.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['dist/spec.button.js']
-};
--- angular-material/dist/spec.input.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-//use form with ng-model
-// user.name (input) ,
-// user.address2 (input),
-// user.city (input),
-// user.state (input),
-// user.postalCode (input)
-// user.biography (area)
